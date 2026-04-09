62697207f1fa0eb3f5f078080b9e6ead4d400ff8
Dannon Baker <dannon.baker@gmail.com>
2026-04-09 12:18:00 -0400

    Hide thinking_delta from chat panel, only render text output

--- app/src/renderer/app.ts
+++ app/src/renderer/app.ts
@@ -52,7 +52,7 @@ gxypi.onAgentEvent((event) => {
         | undefined;
       if (!evt) break;
 
-      if ((evt.type === "text_delta" || evt.type === "thinking_delta") && evt.delta) {
+      if (evt.type === "text_delta" && evt.delta) {
         chatPanel.appendDelta(evt.delta);
       }
       break;
